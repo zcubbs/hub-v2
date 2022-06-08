a7b571d1f5fad992e3db6e38986be034d5bb9987
zcubbs <zackcubby@gmail.com>
2022-06-08 19:27:11 +0200

    :sparkles: add ui customization options

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/gofiber/fiber/v2 v2.26.0
 	github.com/gofiber/template v1.6.22
+	github.com/joho/godotenv v1.4.0
 	gopkg.in/yaml.v2 v2.4.0
 )
 
--- main.go
+++ main.go
@@ -3,34 +3,66 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html"
+	"github.com/joho/godotenv"
 	"gopkg.in/yaml.v2"
+	"html/template"
 	"hub-v2/models"
 	"io/ioutil"
 	"log"
 	"net/url"
 	"os"
 	"strconv"
+	"time"
 )
+
+func init() {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+}
 
 func main() {
 	groups := loadGroupsFromYaml()
 	title := getTitle()
 	disclaimer := getDisclaimer()
+	customHTML := getCustomHtml()
+	showGithub := getShowGithub()
+	subTitle := getSubTitle()
+	logo := getLogo()
 	engine := html.New("./views", ".html")
 
 	app := fiber.New(fiber.Config{
 		Views:       engine,
 		ViewsLayout: "layouts/main",
 	})
-	app.Static("/css", "./public/css")
-	app.Static("/assets", "./public/assets")
+	app.Static("/css", "./public/css", fiber.Static{
+		Compress:      true,
+		ByteRange:     true,
+		CacheDuration: 10 * time.Second,
+		MaxAge:        3600,
+	})
+	app.Static("/assets", "./public/assets", fiber.Static{
+		Compress:      true,
+		ByteRange:     true,
+		CacheDuration: 10 * time.Second,
+		MaxAge:        3600,
+	})
 
 	engine.Reload(isDevMode())
+
+	log.Printf("Custom HTML: %s\n", customHTML)
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Render("index", fiber.Map{
 			"Title":      title,
+			"SubTitle":   subTitle,
+			"Logo":       logo,
 			"Disclaimer": disclaimer,
+			"ShowGithub": showGithub,
+			"CustomHtml": template.HTML(customHTML),
 			"Groups":     groups,
 		})
 	})
@@ -110,10 +142,42 @@ func getDisclaimer() string {
 	return appTitle
 }
 
+func getCustomHtml() string {
+	customHtml := os.Getenv("APP_CUSTOM_HTML")
+	if customHtml == "" {
+		customHtml = "<span></span>"
+	}
+	return customHtml
+}
+
 func getTitle() string {
 	appTitle := os.Getenv("APP_TITLE")
 	if appTitle == "" {
 		appTitle = "z/HuB"
 	}
 	return appTitle
+}
+
+func getShowGithub() bool {
+	showGithub := os.Getenv("SHOW_GITHUB")
+	if showGithub == "false" {
+		return false
+	}
+	return true
+}
+
+func getLogo() string {
+	logo := os.Getenv("LOGO_URL")
+	if logo == "" {
+		return "/assets/logo.png"
+	}
+	return logo
+}
+
+func getSubTitle() string {
+	subTitle := os.Getenv("APP_SUB_TITLE")
+	if subTitle == "" {
+		return "HuB"
+	}
+	return subTitle
 }
